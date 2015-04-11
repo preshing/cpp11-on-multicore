4ccc59514a8e1641f149e78608bbf0332e207476
Jeff Preshing <filter-github@preshing.com>
2015-04-11 12:39:59 -0400

    Fix link errors due to missing -pthread on Linux.

--- cmake/BuildSettings.cmake
+++ cmake/BuildSettings.cmake
@@ -5,7 +5,8 @@ if(${MSVC})
     set(CMAKE_EXE_LINKER_FLAGS_RELEASE "${CMAKE_EXE_LINKER_FLAGS_RELEASE} /debug")
 elseif(${UNIX})
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
-    if(NOT ${CYGWIN})   # Don't specify -pthread on Cygwin
+    if(${CYGWIN})   # Don't specify -pthread on Cygwin
+    else()
         set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -pthread")
     endif()
     set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} -g")
